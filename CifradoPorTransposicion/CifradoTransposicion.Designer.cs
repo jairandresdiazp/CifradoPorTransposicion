80f0fda1f06048adf891e725f03502e7bb737c3f
jairandresdiazp <jairandresdiazp@gmail.com>
2017-04-01 22:33:31 -0500

    version 1.0.0

--- CifradoPorTransposicion/CifradoTransposicion.Designer.cs
+++ CifradoPorTransposicion/CifradoTransposicion.Designer.cs
@@ -156,6 +156,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_Input);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(558, 506);
+            this.MinimumSize = new System.Drawing.Size(558, 506);
             this.Name = "CifradoTransposicion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CifradoTransposicion";
